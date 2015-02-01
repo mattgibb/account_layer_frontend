466b2a8354ccf48f57af69bdbac19f4eff76b48f
Matt Gibb <matt@mattgibb.com>
2015-02-01 11:46:55 -0800

    Stop checking in node_modules

--- node_modules/6to5-core/lib/6to5/transformation/transformers/validation/undeclared-variable-check.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-exports.optional = true;
-
-exports.Identifier = function (node, parent, scope, context, file) {
-  if (!scope.has(node.name, true)) {
-    throw file.errorWithNode(node, "Reference to undeclared variable", ReferenceError);
-  }
-};
