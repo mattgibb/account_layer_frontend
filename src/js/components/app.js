cbf4e996cd880ed0555b2717995f0b289bb8427c
Matt Gibb <matt@mattgibb.com>
2015-02-01 21:23:44 -0800

    Routes work

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,16 +1,18 @@
-React = require('react')
-NavBar = require('./nav_bar')
+var React = require('react')
+var RouteHandler = require('react-router').RouteHandler;
+var NavBar = require('./nav_bar')
 
 function content() {
   return <div>Yeeeeaaah</div>
   // return div({}, FilterableTable({}))
 }
 
-App = React.createClass({
+var App = React.createClass({
   render: function() {
     return <div className="bum">
       <NavBar />
       <div className="container">{content()}</div>
+      <RouteHandler/>
     </div>
   }
 })
--- src/js/init.js
+++ src/js/init.js
@@ -1,8 +1,20 @@
-App = require('./components/app')
-React = require('react')
+var App = require('./components/app')
+var React = require('react')
+var Router = require('react-router');
+var { Route, RouteHandler, Link, DefaultRoute } = Router;
 
 React.initializeTouchEvents(true)
 
-Router.run(routes, Router.HistoryLocation, function(Handler) {
-  React.render(<Handler/>, document.body)
+var routes =
+  <Route handler={App} path="/">
+  </Route>
+  // <DefaultRoute handler={Home} />
+  // <Route name="about" handler={About} />
+  // <NotFoundRoute handler={NotFound}/>
+;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  Router.run(routes, Router.HistoryLocation, function(Handler, state) {
+    React.render(<Handler/>, document.body)
+  });
 })
